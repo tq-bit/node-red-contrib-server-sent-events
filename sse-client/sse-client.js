110309b0a5cfac05bbde131f5abf3ea68f1087df
Tq-bit <t.quante@outlook.com>
2023-06-02 12:48:44 +0200

    feat: add message events

--- sse-client/sse-client.js
+++ sse-client/sse-client.js
@@ -1,15 +1,26 @@
 const EventSource = require("eventsource")
+
+function handleEventSourceError(RED, node, err) {
+  RED.log.error(err);
+  node.send({
+    _msid: RED.util.generateId(),
+    error: err
+  });
+  node.error(err);
+}
+
 module.exports = function (RED) {
 
   function CreateSseClientNode(config) {
     RED.nodes.createNode(this, config);
 
     this.url = config.url
+    this.event = config.event;
     this.eventSource = new EventSource(this.url, { withCredentials: true });
 
     // Register default message event
-    this.eventSource.on('message', (event) => {
-      RED.log.debug(`Received event: ${event}`);
+    this.eventSource.on(this.event, (event) => {
+      RED.log.info(`Received event: ${event}`);
       this.send({
         _msid: RED.util.generateId(),
         topic: event.type,
@@ -20,14 +31,7 @@ module.exports = function (RED) {
     // TODO: Register other event names
 
     // Register error event
-    this.eventSource.on('error', (err) => {
-      RED.log.error(err);
-      this.send({
-        _msid: RED.util.generateId(),
-        error: err
-      });
-      this.error(err);
-    })
+    this.eventSource.on('error', (err) => handleEventSourceError(RED, this, err));
 
     // Register close event of the node runtime to clean up old event sources
     this.on('close', () => {
