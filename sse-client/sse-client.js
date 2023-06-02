701facb6c49753f413e59a3bf72c22dbd799cca1
Tq-bit <t.quante@outlook.com>
2023-06-02 12:41:42 +0200

    chore: add error handler

--- sse-client/sse-client.js
+++ sse-client/sse-client.js
@@ -7,6 +7,7 @@ module.exports = function (RED) {
     this.url = config.url
     this.eventSource = new EventSource(this.url, { withCredentials: true });
 
+    // Register default message event
     this.eventSource.on('message', (event) => {
       RED.log.debug(`Received event: ${event}`);
       this.send({
@@ -16,8 +17,19 @@ module.exports = function (RED) {
       });
     })
 
+    // TODO: Register other event names
 
+    // Register error event
+    this.eventSource.on('error', (err) => {
+      RED.log.error(err);
+      this.send({
+        _msid: RED.util.generateId(),
+        error: err
+      });
+      this.error(err);
+    })
 
+    // Register close event of the node runtime to clean up old event sources
     this.on('close', () => {
       RED.log.debug(`Closing event source: ${this.url}`);
       this.eventSource.close()
