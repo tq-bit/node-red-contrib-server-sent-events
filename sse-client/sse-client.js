c6d00582a1850e66c8bbb8c59dfe8a4f2d940671
Tq-bit <t.quante@outlook.com>
2023-06-02 12:37:56 +0200

    feat: add bare sse client node

--- sse-client/sse-client.js
+++ sse-client/sse-client.js
@@ -1,12 +1,28 @@
+const EventSource = require("eventsource")
 module.exports = function (RED) {
 
   function CreateSseClientNode(config) {
     RED.nodes.createNode(this, config);
-    this.subscribers = [];
-    this.event = config.event;
-    this.data = config.data;
 
-    this.on('input', (msg, send, done) => {});
+    this.url = config.url
+    this.eventSource = new EventSource(this.url, { withCredentials: true });
+
+    this.eventSource.on('message', (event) => {
+      RED.log.debug(`Received event: ${event}`);
+      this.send({
+        _msid: RED.util.generateId(),
+        topic: event.type,
+        payload: event.data
+      });
+    })
+
+
+
+    this.on('close', () => {
+      RED.log.debug(`Closing event source: ${this.url}`);
+      this.eventSource.close()
+    })
+
   }
   RED.nodes.registerType('sse-client', CreateSseClientNode);
 };
