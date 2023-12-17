a0af8b04070f187a3f81e71ea88a32d162a557be
Tq-bit <t.quante@outlook.com>
2023-12-17 20:22:58 +0100

    add conditional check for flush

--- sse-server/sse-server.js
+++ sse-server/sse-server.js
@@ -4,11 +4,11 @@
  * @param {Object} node - The node to update status for.
  */
 function updateNodeStatus(node, type) {
-  node.status({
-    fill: type === "success" ? 'green' : 'red',
-    shape: 'dot',
-    text: `${node.subscribers.length} client(s) connected`,
-  });
+	node.status({
+		fill: type === 'success' ? 'green' : 'red',
+		shape: 'dot',
+		text: `${node.subscribers.length} client(s) connected`,
+	});
 }
 
 /**
@@ -22,38 +22,39 @@ function updateNodeStatus(node, type) {
  * @return {void}
  */
 function registerSubscriber(RED, node, msg) {
-  RED.log.debug('Client connected')
-  // Write the opening header
-  msg.res._res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    Connection: 'keep-alive',
-  });
+	RED.log.debug('Client connected');
+	// Write the opening header
+	msg.res._res.writeHead(200, {
+		'Content-Type': 'text/event-stream',
+		'Cache-Control': 'no-cache',
+		Connection: 'keep-alive',
+	});
 
-  // Write the initial opening message
-  msg.res._res.write('event: open\n');
-  msg.res._res.write(`data: ${JSON.stringify(msg.payload || 'Connection opened')}\n`);
-  msg.res._res.write(`id: ${msg._msgid}\n\n`);
-  msg.res._res.flush();
-  
-  // Close a SSE connection when client disconnects
-  msg.res._res.req.on('close', () => {
-    updateNodeStatus(node, 'success');
-    unregisterSubscriber(node, msg);
-  });
+	// Write the initial opening message
+	msg.res._res.write('event: open\n');
+	msg.res._res.write(`data: ${JSON.stringify(msg.payload || 'Connection opened')}\n`);
+	msg.res._res.write(`id: ${msg._msgid}\n\n`);
+	console.log(msg.res);
+	if (msg.res._res.flush) msg.res._res?.flush();
 
-  // Close all connections when a runtime event is registered, e.g. redeploy
-  RED.events.on('runtime-event', () => {
-    updateNodeStatus(node, 'success');
-    unregisterSubscriber(node, msg);
-  });
+	// Close a SSE connection when client disconnects
+	msg.res._res.req.on('close', () => {
+		updateNodeStatus(node, 'success');
+		unregisterSubscriber(node, msg);
+	});
 
-  // Append the request to a list of subscribers for this nod.
-  node.subscribers.push({
-    id: msg._msgid,
-    socket: msg.res,
-  });
-  updateNodeStatus(node, 'success');
+	// Close all connections when a runtime event is registered, e.g. redeploy
+	RED.events.on('runtime-event', () => {
+		updateNodeStatus(node, 'success');
+		unregisterSubscriber(node, msg);
+	});
+
+	// Append the request to a list of subscribers for this nod.
+	node.subscribers.push({
+		id: msg._msgid,
+		socket: msg.res,
+	});
+	updateNodeStatus(node, 'success');
 }
 
 /**
@@ -65,20 +66,18 @@ function registerSubscriber(RED, node, msg) {
  * @return {void}
  */
 function unregisterSubscriber(node, msg) {
-  // Write out closing message to client
-  msg.res._res.write('event: close\n');
-  msg.res._res.write(`data: The connection was closed by the server.\n`);
-  msg.res._res.write(`id: ${msg._msgid}\n\n`);
-  msg.res._res.flush();
-  
-  // Remove the subscriber from the list
-  node.subscribers = node.subscribers.filter((subscriber) => {
-    return subscriber.id !== msg._msgid;
-  });
-  msg.res._res.end();
+	// Write out closing message to client
+	msg.res._res.write('event: close\n');
+	msg.res._res.write(`data: The connection was closed by the server.\n`);
+	msg.res._res.write(`id: ${msg._msgid}\n\n`);
+	if (msg.res._res.flush) msg.res._res?.flush();
+
+	// Remove the subscriber from the list
+	node.subscribers = node.subscribers.filter((subscriber) => {
+		return subscriber.id !== msg._msgid;
+	});
+	msg.res._res.end();
 }
-
-
 
 /**
  * Sends server event data to all subscribers.
@@ -87,47 +86,47 @@ function unregisterSubscriber(node, msg) {
  * @param {object} msg - The message object containing topic and payload.
  */
 function handleServerEvent(RED, node, msg) {
-  const event = `${node.event || msg.topic || 'message'}`;
-  const data = `${JSON.stringify(node.data || msg.payload)}`;
-  RED.log.info(`Sent event: ${event}`);
-  RED.log.debug(`Data: ${data}`);
-  node.subscribers.forEach((subscriber) => {
-    subscriber.socket._res.write(`event: ${event}\n`);
-    subscriber.socket._res.write(`data: ${data}\n`);
-    subscriber.socket._res.write(`id: ${msg._msgid}\n\n`);
-    subscriber.socket._res.flush();
-  });
+	const event = `${node.event || msg.topic || 'message'}`;
+	const data = `${JSON.stringify(node.data || msg.payload)}`;
+	RED.log.info(`Sent event: ${event}`);
+	RED.log.debug(`Data: ${data}`);
+	node.subscribers.forEach((subscriber) => {
+		subscriber.socket._res.write(`event: ${event}\n`);
+		subscriber.socket._res.write(`data: ${data}\n`);
+		subscriber.socket._res.write(`id: ${msg._msgid}\n\n`);
+		if (subscriber.socket._res.flush) subscriber.socket._res?.flush();
+	});
 }
 
 module.exports = function (RED) {
-  /**
-   * Creates a new SSE (Server-Sent Events) server node with the specified configuration.
-   *
-   * @param {Object} config - the configuration object for the node
-   * @param {string} config.event - the name of the event to emit to the client
-   * @param {string} config.data - the data to send to the client with the event
-   * @return {void}
-   */
-  function CreateSseServerNode(config) {
-    RED.nodes.createNode(this, config);
-    this.subscribers = [];
-    this.event = config.event;
-    this.data = config.data;
+	/**
+	 * Creates a new SSE (Server-Sent Events) server node with the specified configuration.
+	 *
+	 * @param {Object} config - the configuration object for the node
+	 * @param {string} config.event - the name of the event to emit to the client
+	 * @param {string} config.data - the data to send to the client with the event
+	 * @return {void}
+	 */
+	function CreateSseServerNode(config) {
+		RED.nodes.createNode(this, config);
+		this.subscribers = [];
+		this.event = config.event;
+		this.data = config.data;
 
-    this.on('input', (msg, send, done) => {
-      try {
-        if (msg.res) {
-          registerSubscriber(RED, this, msg);
-        } else {
-          handleServerEvent(RED, this, msg);
-        }
-      } catch (error) {
-        RED.log.error(error)
-        updateNodeStatus(this, 'error');
-      } finally {
-        if (done && typeof done === 'function') done()
-      }
-    });
-  }
-  RED.nodes.registerType('sse-server', CreateSseServerNode);
+		this.on('input', (msg, send, done) => {
+			try {
+				if (msg.res) {
+					registerSubscriber(RED, this, msg);
+				} else {
+					handleServerEvent(RED, this, msg);
+				}
+			} catch (error) {
+				RED.log.error(error);
+				updateNodeStatus(this, 'error');
+			} finally {
+				if (done && typeof done === 'function') done();
+			}
+		});
+	}
+	RED.nodes.registerType('sse-server', CreateSseServerNode);
 };
